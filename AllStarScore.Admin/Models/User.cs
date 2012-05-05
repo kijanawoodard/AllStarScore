3ee6d7445945b28d6681578b929650c8226f59ae
Kijana Woodard <github@wyldeye.com>
2012-05-05 16:24:21 -0500

    adjustments to user

--- AllStarScore.Admin/Models/User.cs
+++ AllStarScore.Admin/Models/User.cs
@@ -15,38 +15,32 @@ namespace AllStarScore.Admin.Models
 		public bool Enabled { get; set; }
         
         const string ConstantSalt = "H+iur}H+|tw)/59";
-		protected string HashedPassword { get; private set; }
-		
-        private string _passwordSalt;
-		private string PasswordSalt
-		{
-			get
-			{
-				return _passwordSalt ?? (_passwordSalt = Guid.NewGuid().ToString("N"));
-			}
-			set { _passwordSalt = value; } //required when hydrating back from Raven
-		}
+		protected string HashedPassword { get; private set; }	
+        private string PasswordSalt { get; set; }
 
-		public User SetPassword(string pwd)
+        public User()
+        {
+            PasswordSalt = Guid.NewGuid().ToString("N"); //factory value; thrown away when loaded from db
+        }
+
+        public User SetPassword(string pwd)
 		{
 			HashedPassword = GetHashedPassword(pwd);
 			return this;
 		}
 
-		private string GetHashedPassword(string pwd)
-		{
-			using (var sha = SHA256.Create())
-			{
-				var computedHash = sha.ComputeHash(Encoding.Unicode.GetBytes(PasswordSalt + pwd + ConstantSalt));
-				return Convert.ToBase64String(computedHash);
-			}
-		}
-
 		public bool ValidatePassword(string maybePwd)
 		{
-			if (HashedPassword == null)
-				return true;
 			return HashedPassword == GetHashedPassword(maybePwd);
 		}
+
+        private string GetHashedPassword(string pwd)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var computedHash = sha.ComputeHash(Encoding.Unicode.GetBytes(PasswordSalt + pwd + ConstantSalt));
+                return Convert.ToBase64String(computedHash);
+            }
+        }
     }
 }
