d54f8a2b751774bab59e4dbc72507e51e36bcddf
Kijana Woodard <github@wyldeye.com>
2012-05-19 10:31:45 -0500

    go back to using nuget version of moth

--- AllStarScore.Admin/Global.asax.cs
+++ AllStarScore.Admin/Global.asax.cs
@@ -102,18 +102,18 @@ namespace AllStarScore.Admin
             /* tah dah */
         }
 
-        public override IOutputCacheRestrictions Enable
-        {
-            get
-            {
-                return new OutputCacheRestrictions()
-                {
-                    PageOutput = true, 
-                    CssTidy = false,   
-                    ScriptMinification = false,
-                    CssPreprocessing = true 
-                };
-            }
-        }
+//        public override IOutputCacheRestrictions Enable
+//        {
+//            get
+//            {
+//                return new OutputCacheRestrictions()
+//                {
+//                    PageOutput = true, 
+//                    CssTidy = false,   
+//                    ScriptMinification = false,
+//                    CssPreprocessing = true 
+//                };
+//            }
+//        }
     }
 }
