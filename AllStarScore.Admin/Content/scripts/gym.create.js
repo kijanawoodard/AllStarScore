a58b23a77189a9bedd49c9b6b83aff9df3523706
Kijana Woodard <github@wyldeye.com>
2012-05-20 20:24:22 -0500

    progress on gym creation

--- AllStarScore.Admin/Content/scripts/gym.create.js
+++ AllStarScore.Admin/Content/scripts/gym.create.js
@@ -1,27 +1,33 @@
 ﻿$(document).ready(function () {
-    $('#Name').autocomplete({
-        source: function (request, response) {
-            // define a function to call your Action (assuming UserController)
-            var url = $('#gym_search').attr('href');
-            $.ajax({
-                url: url,
-                type: "GET",
-                dataType: "json",
+        
+    $.subscribe('/gym/form/loaded', function (event) {
+        $('#Name').autocomplete({
+            source: function (request, response) {
+                // define a function to call your Action (assuming UserController)
+                var url = $('#gym_search').attr('href');
+                $.ajax({
+                    url: url,
+                    type: "GET",
+                    dataType: "json",
 
-                // query will be the param used by your action method
-                data: { query: request.term },
-                success: function (data) {
-                    response($.map(data, function (item) {
-                        return { label: item.Name + ' from ' + item.Location, value: item.Name, id: item.Id };
-                    }));
-                }
-            });
-        },
-        minLength: 1,
-        change: function (event, ui) {
-            console.log(ui.item ?
-					"Selected: " + ui.item.value + " aka " + ui.item.id :
-					"Nothing selected, input was " + this.value);
-        }
+                    // query will be the param used by your action method
+                    data: { query: request.term },
+                    success: function (data) {
+                        response($.map(data, function (item) {
+                            return { label: item.Name + ' from ' + item.Location, value: item.Name, id: item.Id };
+                        }));
+                    }
+                });
+            },
+            minLength: 1,
+            change: function (event, ui) {
+                console.log(ui.item ?
+    					"Selected: " + ui.item.value + " aka " + ui.item.id :
+    					"Nothing selected, input was " + this.value);
+            }
+        });
     });
+    
+    $('#gym_create').onClickAjaxInto('#gym');
+    //$('#gym_create').click();
 });    
