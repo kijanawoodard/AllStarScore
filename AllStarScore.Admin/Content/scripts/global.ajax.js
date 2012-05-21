007c2837fefa7c933fccf762bea99304156b313c
Kijana Woodard <github@wyldeye.com>
2012-05-21 00:22:52 -0500

    improved jquery plugins and working out ajax strategy

--- AllStarScore.Admin/Content/scripts/global.ajax.js
+++ AllStarScore.Admin/Content/scripts/global.ajax.js
@@ -1,46 +1,58 @@
-﻿$.fn.onClickAjaxInto = function (target) {
-    this.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: this.href,
-            type: 'get',
-            success: function (result) {
-                $(target).html(result);
-            }
+﻿(function ($) {
+    $.fn.onClickAjaxInto = function (target) {
+        return this.each(function() {
+            $(this).click(function(event) {
+                event.preventDefault();
+                $.ajax({
+                    url: this.href,
+                    type: 'get',
+                    success: function(result) {
+                        $(target).html(result);
+                    }
+                });
+                return false;
+            });
         });
-        return false;
-    });
-};
+    };
+})(jQuery);
 
-$.fn.onSubmitAjaxInto = function (target) {
-    this.submit(function (event) {
-        event.preventDefault();
-        if ($(this).valid()) {
-            $.ajax({
-                url: this.action,
-                type: this.method,
-                data: $(this).serialize(),
-                success: function (result) {
-                    $(target).html(result);
+(function($) {
+    $.fn.onSubmitAjaxInto = function(target) {
+        return this.each(function() {
+            $(this).submit(function(event) {
+                event.preventDefault();
+                if ($(this).valid()) {
+                    $.ajax({
+                        url: this.action,
+                        type: this.method,
+                        data: $(this).serialize(),
+                        success: function(result) {
+                            $(target).html(result);
+                        }
+                    });
+                }
+                return false;
+            });
+        });
+    };
+})(jQuery);
+
+(function($) {
+    $.fn.pickDateBefore = function(last) {
+        return this.each(function() {
+            var first = $(this);
+            first.datepicker({
+                numberOfMonths: 2,
+                onSelect: function(selected) {
+                    $(last).datepicker("option", "minDate", selected);
                 }
             });
-        }
-        return false;
-    });
-};
-
-$.fn.pickDateBefore = function (last) {
-    var first = this;
-    first.datepicker({
-        numberOfMonths: 2,
-        onSelect: function (selected) {
-            $(last).datepicker("option", "minDate", selected);
-        }
-    });
-    $(last).datepicker({
-        numberOfMonths: 2,
-        onSelect: function (selected) {
-            first.datepicker("option", "maxDate", selected);
-        }
-    });
-};
+            $(last).datepicker({
+                numberOfMonths: 2,
+                onSelect: function(selected) {
+                    first.datepicker("option", "maxDate", selected);
+                }
+            });
+        });
+    };
+})(jQuery);
--- AllStarScore.Admin/Content/scripts/gym.create.js
+++ AllStarScore.Admin/Content/scripts/gym.create.js
@@ -1,33 +1,37 @@
 ﻿$(document).ready(function () {
-        
-    $.subscribe('/gym/form/loaded', function (event) {
-        $('#Name').autocomplete({
-            source: function (request, response) {
-                // define a function to call your Action (assuming UserController)
-                var url = $('#gym_search').attr('href');
-                $.ajax({
-                    url: url,
-                    type: "GET",
-                    dataType: "json",
+    $('a.gym_create')
+        .onClickAjaxInto('.gym_create .display')
+        .click();
+});
 
-                    // query will be the param used by your action method
-                    data: { query: request.term },
-                    success: function (data) {
-                        response($.map(data, function (item) {
-                            return { label: item.Name + ' from ' + item.Location, value: item.Name, id: item.Id };
-                        }));
-                    }
-                });
-            },
-            minLength: 1,
-            change: function (event, ui) {
-                console.log(ui.item ?
-    					"Selected: " + ui.item.value + " aka " + ui.item.id :
-    					"Nothing selected, input was " + this.value);
-            }
-        });
+
+$.subscribe('/gym/create/form/loaded', function () {
+
+    $('#Name').autocomplete({
+        source: function (request, response) {
+            var url = $('.gym_search').attr('href');
+            $.ajax({
+                url: url,
+                type: "GET",
+                dataType: "json",
+
+                data: { query: request.term },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return { label: item.Name + ' from ' + item.Location, value: item.Name, id: item.Id };
+                    }));
+                }
+            });
+        },
+        minLength: 1,
+        select: function (event, ui) {
+            console.log(ui.item ?
+                    "Selected: " + ui.item.value + " aka " + ui.item.id :
+                    "Nothing selected, input was " + this.value);
+        }
     });
-    
-    $('#gym_create').onClickAjaxInto('#gym');
-    //$('#gym_create').click();
-});    
+
+    $('#Name').bind('input propertychange', function() {
+        console.log($(this).val());
+    });
+});
