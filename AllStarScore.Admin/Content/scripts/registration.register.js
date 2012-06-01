7394f0e6f00dc6f8449ef64811affde46b8830bb
Kijana Woodard <github@wyldeye.com>
2012-06-01 12:25:04 -0500

    cleaning up

--- AllStarScore.Admin/Content/scripts/registration.register.js
+++
@@ -1,6 +0,0 @@
-﻿$.subscribe('/gym/selected', function (event, gymid) {
-    var target = $('.team_registration_harness .control_links .form_get');
-    var value = target.attr('href').replace('__gymid__', gymid);
-    target.attr('href', value);
-    target.click();
-});
