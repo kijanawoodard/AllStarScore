46a2984593c488f10cc2db7e930cebe6d3f69c17
Kijana Woodard <github@wyldeye.com>
2012-10-07 18:10:44 -0500

    high medium low guidance ajaxed

--- AllStarScore.Scoring/AllStarScore.Scoring/Content/scripts/scoring.highmediumlow.js
+++ AllStarScore.Scoring/AllStarScore.Scoring/Content/scripts/scoring.highmediumlow.js
@@ -1,48 +1,50 @@
 ﻿$(document).ready(function () {
     //console.log(AllStarScore);
-    $.getJSON(window.dataLink, function (data) {
-        console.log(data);
-        //window.highMediumLowData = JSON.parse(window.NormalizeLineEndings(data));
-    });
-
-    AllStarScore.HighMediumLow = new AllStarScore.HighMediumLow({ panelJudges: [], panel: {calculator: {scores: []}} });
+    AllStarScore.HighMediumLow = new AllStarScore.HighMediumLow();
 });
 
-AllStarScore.HighMediumLow = function (data) {
+AllStarScore.HighMediumLow = function () {
     var self = this;
 
     self.panelJudges = AllStarScore.CompetitionData.panelJudges;
     var performance = AllStarScore.ScoreEntry.performance;
-    var judgeScores = data.panel.calculator.scores;
+    self.judgeScores = ko.observableArray();
     var division = performance.divisionIdWithoutCompanyId;
     var level = performance.levelIdWithoutCompanyId;
     var map = AllStarScore.ScoringMap.categories[division] || AllStarScore.ScoringMap.categories[level];
 
     self.panel = performance.panel;
-    self.comments = $.map(judgeScores, function (score) {
-        return { judgeId: score.judgeId, comment: score.comments };
-    });
+    
+    self.categories = ko.computed(function () {
+        var result = $.map(map, function (category, key) {
+            var scores = {};
 
-    self.categories = $.map(map, function (category, key) {
-        var scores = {};
+            _.each(self.judgeScores(), function (judge) {
+                var ok = _.contains(self.panelJudges, judge.judgeId);
+                if (ok) {
+                    scores[judge.judgeId] = judge.scores[key] ? judge.scores[key] : { base: 0.0, execution: 0.0, total: 0.0 };
+                    var score = scores[judge.judgeId];
+                    var base = score.base % 1; //http://stackoverflow.com/a/4512317/214073
+                    score.isLow = base > 0 && base <= .3;
+                    score.isMedium = base > .3 && base <= .6;
+                    score.isHigh = base > .6 && base <= .9;
+                    score.display = ko.observable(score.isLow ? "Low " : score.isMedium ? "Medium " : score.isHigh ? "High " : "");
+                }
+            });
 
-        _.each(judgeScores, function (judge) {
-            var ok = _.contains(self.panelJudges, judge.judgeId);
-            if (ok) {
-                scores[judge.judgeId] = judge.scores[key] ? judge.scores[key] : { base: 0.0, execution: 0.0, total: 0.0 };
-                var val = scores[judge.judgeId];
-                var base = val.base % 1; //http://stackoverflow.com/a/4512317/214073
-                scores[judge.judgeId].isLow = base > 0 && base <= .3;
-                scores[judge.judgeId].isMedium = base > .3 && base <= .6;
-                scores[judge.judgeId].isHigh = base > .6 && base <= .9;
-                scores[judge.judgeId].display = ko.observable(val.isLow ? "Low " : val.isMedium ? "Medium " : val.isHigh ? "High " : "");
-            }
+            return { key: key, display: category.display, scores: AllStarScore.Utilities.asArray(scores) };
         });
 
-        return { key: key, display: category.display }; //, scores: AllStarScore.Utilities.asArray(scores) };
-    });
+        return result;
+    }, self);
 
     $('#scoring_highmediumlow').on('click', 'button', function () {
-        AllStarScore.HighMediumLow.categories[0].scores[0].display("HAHAHAHHA");
+        $.getJSON(AllStarScore.HighMediumLowLink, function (data) {
+            //ko.mapping.fromJS(data.panel.calculator.scores, {}, self.judgeScores); //the observable elements introduced too much overhead in the computed method here
+            self.judgeScores.removeAll();
+            _.each(data.panel.calculator.scores, function(score) {
+                self.judgeScores.push(score);
+            });
+        });
     });
 };
