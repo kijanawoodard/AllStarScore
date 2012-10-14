be58b2621f6655ba44a04b87db41616f67619acf
Kijana Woodard <github@wyldeye.com>
2012-10-14 17:36:32 -0500

    add nuspec to scoring

--- AllStarScore.Scoring/AllStarScore.Scoring/Properties/AssemblyInfo.cs
+++ AllStarScore.Scoring/AllStarScore.Scoring/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AllStarScore.Scoring")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Scoring Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AllStarScore.com")]
 [assembly: AssemblyProduct("AllStarScore.Scoring")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
