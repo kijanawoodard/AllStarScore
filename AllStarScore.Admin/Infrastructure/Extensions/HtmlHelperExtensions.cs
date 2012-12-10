192cb7038e9384a151252602265f29bb74683466
Kijana Woodard <github@wyldeye.com>
2012-12-10 14:28:14 -0600

    a different way to deal with json

---
+++ AllStarScore.Admin/Infrastructure/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using AllStarScore.Extensions;
+
+namespace AllStarScore.Admin.Infrastructure.Extensions
+{
+	public static class HtmlHelperExtensions
+	{
+		public static IHtmlString EncodeAsJson<T>(this HtmlHelper helper, T model)
+		{
+			//ToJson lower cases the properties so they are more javascript-y
+			//encode takes care of quotes, line breaks, etc
+			//raw so that it is not treated as html
+			return helper.Raw(Json.Encode(model.ToJson()));
+		}
+	}
+}
